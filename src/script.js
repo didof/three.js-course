a5b4da297112f815d9d568bcc1f81472f0f02ede
didof <didonato.fr@gmail.com>
2021-04-18 15:54:22 +0200

    added controls, resizing, fullscreen

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const canvas = document.getElementById('canvas')
 const sizes = {
@@ -21,12 +22,48 @@ scene.add(mesh)
 const camera = new THREE.PerspectiveCamera(75, sizes.getRatio(), 0.1, 100)
 camera.position.z = 2
 
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.update()
+
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true })
 renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio()
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  camera.aspect = sizes.getRatio()
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(sizes.width, sizes.height)
+
+  // in case application is moved across screens with different pixel ratios
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+window.addEventListener('dblclick', () => {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen()
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen()
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen()
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen()
+    }
+  }
+})
 
 const clock = new THREE.Clock()
 const tick = () => {
+  controls.update()
   const et = clock.getElapsedTime()
 
   mesh.rotation.y = et
